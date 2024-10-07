2142043ce874602391c02582c0c1fefd10d4f444
David Fernandez <josfernandez@estudiantec.cr>
2024-10-06 21:03:17 -0600

    Test validation

--- mern-login/.ipynb_checkpoints/mainTest-checkpoint.py
+++ mern-login/.ipynb_checkpoints/mainTest-checkpoint.py
@@ -8,19 +8,18 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 
-
 class MyProjectTest(unittest.TestCase):
     def setUp(self):
         chromedriver_path = r"C:\Users\dfern\OneDrive\Escritorio\chromedriver.exe"
         service = Service(chromedriver_path)
         self.driver = webdriver.Chrome(service=service)
 
-    def test_Validatelogin(self):  # Test 1 verificar que la ruta funcione
+    def test_Validatelogin(self): #Test 1 verificar que la ruta funcione
         url = "http://localhost:3000/"
-
+        
         response = requests.get(url)
         self.assertEqual(response.status_code, 200, "No se pudo acceder al link.")
-
+        
         self.driver.get(url)
         time.sleep(3)  # Espera 3 segundos para observar los resultados
 
@@ -28,63 +27,47 @@ class MyProjectTest(unittest.TestCase):
         self.driver.get("http://localhost:3000/")
 
         name_input = self.driver.find_element(By.CSS_SELECTOR, 'input[type="text"]')
-        password_input = self.driver.find_element(
-            By.CSS_SELECTOR, 'input[type="password"]'
-        )
-        submit_button = self.driver.find_element(
-            By.CSS_SELECTOR, 'button[type="submit"]'
-        )
+        password_input = self.driver.find_element(By.CSS_SELECTOR, 'input[type="password"]')
+        submit_button = self.driver.find_element(By.CSS_SELECTOR, 'button[type="submit"]')
 
-        name_input.send_keys("david")
-        password_input.send_keys("1234")
+        name_input.send_keys("david")  
+        password_input.send_keys("1234")  
 
         submit_button.click()
 
         WebDriverWait(self.driver, 10).until(
-            EC.url_contains(
-                "/authenticated"
-            )  # Verifica que la URL contenga "/authenticated"
+            EC.url_contains("/authenticated")  # Verifica que la URL contenga "/authenticated"
         )
-
     def test_login_with_invalid_user(self):
         self.driver.get("http://localhost:3000/")
 
         initial_url = self.driver.current_url
         name_input = self.driver.find_element(By.CSS_SELECTOR, 'input[type="text"]')
-        password_input = self.driver.find_element(
-            By.CSS_SELECTOR, 'input[type="password"]'
-        )
-        submit_button = self.driver.find_element(
-            By.CSS_SELECTOR, 'button[type="submit"]'
-        )
+        password_input = self.driver.find_element(By.CSS_SELECTOR, 'input[type="password"]')
+        submit_button = self.driver.find_element(By.CSS_SELECTOR, 'button[type="submit"]')
 
-        name_input.send_keys("InvalidUser")
-        password_input.send_keys("402")
+        name_input.send_keys("InvalidUser")  
+        password_input.send_keys("402")  
 
         submit_button.click()
 
         # Esperar a que aparezca el mensaje de error en la página
-        WebDriverWait(self.driver, 4).until(EC.url_to_be(initial_url))
+        WebDriverWait(self.driver, 4).until(
+            EC.url_to_be(initial_url)
+        )
 
         # Obtener la URL actual después del clic
         current_url = self.driver.current_url
-
+    
         # Verificar si la URL sigue siendo la misma
-        self.assertEqual(
-            initial_url,
-            current_url,
-            "Error: The URL changed unexpectedly, login might have succeeded incorrectly.",
-        )
+        self.assertEqual(initial_url, current_url, "Error: The URL changed unexpectedly, login might have succeeded incorrectly.")
+
 
     def test_logout(self):
-        self.driver.get(
-            "http://localhost:3000/authenticated"
-        )  # Asegúrate de que el usuario esté autenticado
+        self.driver.get("http://localhost:3000/authenticated")  # Asegúrate de que el usuario esté autenticado
         time.sleep(3)
         # Localiza el botón de "Cerrar sesión"
-        logout_button = self.driver.find_element(
-            By.CLASS_NAME, "regreso"
-        )  # Asegúrate de que la clase del botón sea correcta
+        logout_button = self.driver.find_element(By.CLASS_NAME, 'regreso')  # Asegúrate de que la clase del botón sea correcta
 
         # Haz clic en el botón de "Cerrar sesión"
         logout_button.click()
@@ -96,18 +79,16 @@ class MyProjectTest(unittest.TestCase):
 
         current_url = self.driver.current_url
         self.assertIn("/", current_url)  # Verifica que la URL actual contenga "/"
-
+        # Espera 3 segundos antes de continuar
+        time.sleep(3)actual contenga "/"
+        # Espera 3 segundos antes de continuar
         time.sleep(3)
 
-    def test_logout_Validation(self):
-        self.driver.get(
-            "http://localhost:3000/authenticated"
-        )  # Asegúrate de que el usuario esté autenticado
+      def test_logout_Validation(self):
+        self.driver.get("http://localhost:3000/authenticated")  # Asegúrate de que el usuario esté autenticado
         time.sleep(3)
         # Localiza el botón de "Cerrar sesión"
-        logout_button = self.driver.find_element(
-            By.CLASS_NAME, "regreso"
-        )  # Asegúrate de que la clase del botón sea correcta
+        logout_button = self.driver.find_element(By.CLASS_NAME, 'regreso')  # Asegúrate de que la clase del botón sea correcta
 
         # Haz clic en el botón de "Cerrar sesión"
         logout_button.click()
@@ -122,7 +103,6 @@ class MyProjectTest(unittest.TestCase):
 
     def tearDown(self):
         self.driver.quit()
-
 
 # Ejecuta la prueba directamente en Jupyter
 suite = unittest.TestLoader().loadTestsFromTestCase(MyProjectTest)
